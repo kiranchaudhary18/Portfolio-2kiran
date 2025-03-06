69746d390c9e320053eb443e1211bf24b4be370c
kiranchaudhary18 <kiran.chaudhary.cg@gmail.com>
2025-03-06 13:11:04 +0530

    updated code

--- vite_app/src/components/contact.jsx
+++ vite_app/src/components/contact.jsx
@@ -35,7 +35,7 @@ export default function Contact() {
     setFormStatus("submitting");
   
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("https://portfolio-2kiran-server.onrender.com/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
